f5c79249e392ff5f1d29ef4ecb710f9893c766e5
MC-CLINT <ckvidam@st.ug.edu.gh>
2024-10-15 17:20:37 +0000

    Completed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 // Below is to grab the tab items using their class name
 const tabIcons = document.querySelectorAll(".tab--item")
-const tabContentItem = document.querySelectorAll(".tab--content--item")
+const tabContentItems = document.querySelectorAll(".tab--content--item")
 
 // function
 function selectItem(e){
+    removeBorder();
+    removeShow();
     //adding a border to current tab
     this.classList.add('tab--border')
+
+    // Fetch Content
+    const tabContentItem = document.querySelector(`#${this.id}--content`);
+
+    // Add show class
+    tabContentItem.classList.add('show');
 }
+
+function removeBorder() {
+    tabIcons.forEach(item =>item.classList.remove('tab--border'))
+}
+
+
+function removeShow() {
+    tabContentItems.forEach(item =>item.classList.remove('show'))
+}
+
+
+
+
 
 // listens to the click ... When there's a click on the tab-icon then the tab content shows
 
